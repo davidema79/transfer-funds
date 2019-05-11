c6ddc869c6c1eea6e05d17acad6c06b960517564
Davide Martorana <davide.martorana@ricston.com>
2019-05-11 21:46:46 +0200

    Refactored tests code and added "Dead lock" test.

--- src/test/java/eu/davidemartorana/banking/accounts/AccountRepositoryTest.java
+++ src/test/java/eu/davidemartorana/banking/accounts/AccountRepositoryTest.java
@@ -8,6 +8,7 @@ import io.dropwizard.testing.junit.DAOTestRule;
 import org.assertj.core.api.Assertions;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.exception.LockAcquisitionException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,9 +17,7 @@ import javax.persistence.PessimisticLockException;
 import javax.security.auth.login.AccountLockedException;
 import javax.ws.rs.NotFoundException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Currency;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.*;
 
 /**
@@ -119,16 +118,12 @@ public class AccountRepositoryTest {
 
     }
 
-    @Test
-    public void testCheckAndIncreaseLockingResourceByAccountId_Locking_Test() throws Exception {
 
-        final int threadPoolSize = 3;
-        final int threadsNumber = threadPoolSize -1;
-        final CountDownLatch countDownLatch = new CountDownLatch(threadsNumber);
 
-        final Amount amount = new Amount(new BigDecimal("100.00"), Currency.getInstance("GBP"));
-        final Long accountId = 1L;
-        final Account accountToSave = createAccount(accountId, "any-uuid", Currency.getInstance("GBP"), "150.00");
+    private void commonLockingTests(final Amount amount, final Long accountId, final String initialAmount, final String expectedFinalAmount, final int threadsNumber ,final Callable<Account> callable) throws Exception {
+        final int threadPoolSize = threadsNumber+1;
+
+        final Account accountToSave = createAccount(accountId, "any-uuid", Currency.getInstance("GBP"), initialAmount);
 
         database.inTransaction( () -> {
                     accountDAOHelper.getSession().save(accountToSave);
@@ -141,7 +136,47 @@ public class AccountRepositoryTest {
         final List<Future<Account>> futureList = new ArrayList<>(threadPoolSize);
         for(int index = 0; index < threadsNumber ; index++) {
             final Future<Account> future = executor.submit(() -> {
-                    return database.inTransaction(() -> {
+                return database.inTransaction(callable);
+            });
+            futureList.add(future);
+        }
+
+        Assertions.assertThat(futureList).isNotEmpty().hasSize(threadsNumber);
+
+        int counter =0;
+        try {
+            final Account first = futureList.get(0).get(100, TimeUnit.SECONDS);
+            Assertions.assertThat(first).isNotNull();
+            Assertions.assertThat(first.getTotalAmount()).isEqualByComparingTo(expectedFinalAmount);
+        } catch (final Exception e) {
+            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
+            counter++;
+        }
+
+
+        try {
+            final Account second = futureList.get(1).get(100, TimeUnit.SECONDS);
+            Assertions.assertThat(second).isNotNull();
+            Assertions.assertThat(second.getTotalAmount()).isEqualByComparingTo(expectedFinalAmount);
+        } catch (final Exception e) {
+            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
+            counter++;
+        }
+
+        // Asserting that only one transaction has failed.
+        Assertions.assertThat(counter).isEqualTo(1);
+    }
+
+    @Test
+    public void testCheckAndIncreaseLockingResourceByAccountId_Locking_Test() throws Exception {
+
+        final int threadsNumber = 2;
+        final CountDownLatch countDownLatch = new CountDownLatch(threadsNumber);
+
+        final Amount amount = new Amount(new BigDecimal("100.00"), Currency.getInstance("GBP"));
+        final Long accountId = 1L;
+
+        commonLockingTests(amount, accountId, "150.00", "250.00", threadsNumber,() -> {
                         final Account account;
                         try {
                             account= accountRepository.checkAndIncreaseLockingResourceByAccountId(amount, accountId);
@@ -152,101 +187,132 @@ public class AccountRepositoryTest {
                         }
                         return account;
                     });
-            });
-            futureList.add(future);
-        }
-
-        Assertions.assertThat(futureList).isNotEmpty().hasSize(threadsNumber);
-
-
-
-        int counter =0;
-        try {
-            final Account first = futureList.get(0).get(100, TimeUnit.SECONDS);
-            Assertions.assertThat(first).isNotNull();
-            Assertions.assertThat(first.getTotalAmount()).isEqualByComparingTo("250.00");
-        } catch (final Exception e) {
-            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
-            counter++;
-        }
-
-
-         try {
-            final Account second = futureList.get(1).get(100, TimeUnit.SECONDS);
-             Assertions.assertThat(second).isNotNull();
-             Assertions.assertThat(second.getTotalAmount()).isEqualByComparingTo("250.00");
-        } catch (final Exception e) {
-            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
-             counter++;
-        }
-
-         // Asserting that only one transaction has failed.
-         Assertions.assertThat(counter).isEqualTo(1);
     }
 
     @Test
     public void testCheckAndDecreaseLockingResourceByAccountId_Locking_Test() throws Exception {
 
-        final int threadPoolSize = 3;
-        final int threadsNumber = threadPoolSize -1;
+        final int threadsNumber = 2;
         final CountDownLatch countDownLatch = new CountDownLatch(threadsNumber);
 
         final Amount amount = new Amount(new BigDecimal("100.00"), Currency.getInstance("GBP"));
         final Long accountId = 1L;
-        final Account accountToSave = createAccount(accountId, "any-uuid", Currency.getInstance("GBP"), "150.00");
+
+        commonLockingTests(amount, accountId, "150.00", "50.00", threadsNumber,() -> {
+            final Account account;
+            try {
+                account= accountRepository.checkAndDecreaseLockingResourceByAccountId(amount, accountId);
+            } finally {
+                countDownLatch.countDown();
+                System.out.printf("countDownLatch: %s\n", countDownLatch.getCount());
+                countDownLatch.await();
+            }
+            return account;
+        });
+    }
+
+
+    /**
+     * This test case tests that in case of a race condition occurs the micro-service handles it gracefully: either no
+     * transaction changes the database internal status or just one of them does it. Never both.
+     *
+     *
+     */
+    @Test
+    public void testNoDeadLock_On_FundsTransfer() throws Exception{
+
+        final int threadsNumber = 2;
+        final CountDownLatch countDownLatchFirstThread = new CountDownLatch(2);
+        final CountDownLatch countDownLatchSecondThread = new CountDownLatch(2);
+
+        final Amount amount = new Amount(new BigDecimal("150.00"), Currency.getInstance("GBP"));
+        final Long firstAccountId = 12345L;
+        final Long secondAccountId = 6789L;
+
+        final String firstInitialAmount = "200.00";
+        final String secondInitialAmount = "300.00";
+
+        final Account firstAccountToSave = createAccount(firstAccountId, "any-uuid-1", Currency.getInstance("GBP"), firstInitialAmount);
+        final Account secondAccountToSave = createAccount(secondAccountId, "any-uuid-2", Currency.getInstance("GBP"), secondInitialAmount);
 
         database.inTransaction( () -> {
-                    accountDAOHelper.getSession().save(accountToSave);
+                    accountDAOHelper.getSession().save(firstAccountToSave);
+                    accountDAOHelper.getSession().save(secondAccountToSave);
                     accountDAOHelper.getSession().flush();
                 }
         );
 
-        final ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
+        final ExecutorService executor = Executors.newFixedThreadPool(threadsNumber);
 
-        final List<Future<Account>> futureList = new ArrayList<>(threadPoolSize);
-        for(int index = 0; index < threadsNumber ; index++) {
-            final Future<Account> future = executor.submit(() -> {
-                return database.inTransaction(() -> {
-                    final Account account;
-                    try {
-                        account= accountRepository.checkAndDecreaseLockingResourceByAccountId(amount, accountId);
-                    } finally {
-                        countDownLatch.countDown();
-                        System.out.printf("countDownLatch: %s\n", countDownLatch.getCount());
-                        countDownLatch.await();
-                    }
-                    return account;
-                });
-            });
-            futureList.add(future);
-        }
+        final Callable<Account> firstThread = () -> {
+            final Account account1;
+            final Account account2;
+            try {
+                account2= accountRepository.checkAndDecreaseLockingResourceByAccountId(amount, secondAccountId);
 
-        Assertions.assertThat(futureList).isNotEmpty().hasSize(threadsNumber);
+                countDownLatchFirstThread.countDown();
+                countDownLatchFirstThread.await();
 
+                account1= accountRepository.checkAndIncreaseLockingResourceByAccountId(amount, firstAccountId);
+            } finally {
+                countDownLatchSecondThread.countDown();
+            }
+            countDownLatchSecondThread.await();
+            return account1;
+        };
 
+        final Callable<Account> secondThread = () -> {
+            final Account account1;
+            final Account account2;
+            try {
+                account1= accountRepository.checkAndIncreaseLockingResourceByAccountId(amount, firstAccountId);
 
-        int counter =0;
+                countDownLatchFirstThread.countDown();
+                countDownLatchFirstThread.await();
+
+                account2= accountRepository.checkAndDecreaseLockingResourceByAccountId(amount, secondAccountId);
+            } finally {
+                countDownLatchSecondThread.countDown();
+            }
+            countDownLatchSecondThread.await();
+            return account2;
+        };
+
+        final Map<String, Future<Account>> futureMap = new HashMap<>(threadsNumber);
+        futureMap.put(firstAccountToSave.getUuid(), executor.submit( () -> database.inTransaction(firstThread)));
+        futureMap.put(secondAccountToSave.getUuid(), executor.submit( () -> database.inTransaction(secondThread)));
+
+        int counter = 0;
         try {
-            final Account first = futureList.get(0).get(100, TimeUnit.SECONDS);
-            Assertions.assertThat(first).isNotNull();
-            Assertions.assertThat(first.getTotalAmount()).isEqualByComparingTo("50.00");
+            final Account account = futureMap.get(secondAccountToSave.getUuid()).get(100, TimeUnit.SECONDS);
+            Assertions.assertThat(account).isNotNull();
+            Assertions.assertThat(account.getTotalAmount()).isEqualByComparingTo("150.00");
         } catch (final Exception e) {
-            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
+            Assertions.assertThat(e)
+                    .hasRootCauseExactlyInstanceOf(IllegalStateException.class)
+                    .hasStackTraceContaining("Caused by: java.lang.IllegalStateException: Transaction")
+                    .hasStackTraceContaining("has been chosen as a deadlock victim");
             counter++;
         }
 
-
         try {
-            final Account second = futureList.get(1).get(100, TimeUnit.SECONDS);
-            Assertions.assertThat(second).isNotNull();
-            Assertions.assertThat(second.getTotalAmount()).isEqualByComparingTo("50.00");
+            final Account account = futureMap.get(firstAccountToSave.getUuid()).get(100, TimeUnit.SECONDS);
+            Assertions.assertThat(account).isNotNull();
+            Assertions.assertThat(account.getTotalAmount()).isEqualByComparingTo("350.00");
         } catch (final Exception e) {
-            Assertions.assertThat(e).hasCauseExactlyInstanceOf(PessimisticLockException.class);
+            Assertions.assertThat(e)
+                    .hasRootCauseExactlyInstanceOf(IllegalStateException.class)
+                    .hasStackTraceContaining("Caused by: java.lang.IllegalStateException: Transaction")
+                    .hasStackTraceContaining("has been chosen as a deadlock victim");
             counter++;
         }
 
-        // Asserting that only one transaction has failed.
-        Assertions.assertThat(counter).isEqualTo(1);
+        // Asserting that at least one transaction has failed.
+        Assertions.assertThat(counter).isBetween(1,2);
+
+
+
+
     }
 
 }
